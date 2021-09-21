7086b6b1807f7ba1cc4d70d5544d7fe4e4e7fcf0
Ígor Chagas <igorchgasm@hotmail.com>
2021-09-21 08:28:48 -0300

    Adds take_damage and heal functions

--- lib/dungeon_crawl/character.ex
+++ lib/dungeon_crawl/character.ex
@@ -16,6 +16,33 @@ defmodule DungeonCrawl.Character do
             damage_range: nil
 
   defimpl String.Chars do
+    @spec to_string(DungeonCrawl.Character.t()) :: String.t()
     def to_string(character), do: character.name
   end
+
+  @spec take_damage(DungeonCrawl.Character.t(), number()) :: DungeonCrawl.Character.t()
+  def take_damage(character, damage) do
+    new_hit_points = max(0, character.hit_points - damage)
+    %{character | hit_points: new_hit_points}
+  end
+
+  @spec heal(DungeonCrawl.Character.t(), number()) :: DungeonCrawl.Character.t()
+  def heal(character, healing_value) do
+    new_hit_points =
+      min(
+        character.hit_points + healing_value,
+        character.max_hit_points
+      )
+
+    %{character | hit_points: new_hit_points}
+  end
+
+  # def update_hit_points(character, value, function) do
+  #   new_hit_points = function(character)
+  #   %{character | hit_points: new_hit_points}
+  # end
+
+  @spec current_stats(DungeonCrawl.Character.t()) :: String.t()
+  def current_stats(character),
+    do: "Player Stats\nHP: #{character.hit_points}/#{character.max_hit_points}"
 end
--- test/dungeon_crawl_test.exs
+++ test/dungeon_crawl_test.exs
@@ -1,8 +1,36 @@
 defmodule DungeonCrawlTest do
   use ExUnit.Case
+  alias DungeonCrawl.Character
   doctest DungeonCrawl
+
+  @hero %Character{hit_points: 10, max_hit_points: 15}
 
   test "greets the world" do
     assert DungeonCrawl.hello() == :world
+  end
+
+  test "hero takes damage" do
+    initial_hit_points = @hero.hit_points
+    %Character{hit_points: hit_points} = Character.take_damage(@hero, 3)
+    assert hit_points == initial_hit_points - 3
+  end
+
+  test "hero hit_points dont go under zero" do
+    %Character{hit_points: hit_points} = Character.take_damage(@hero, 10)
+    assert hit_points == 0
+  end
+
+  test "hero heals damage" do
+    initial_hit_points = @hero.hit_points
+    %Character{hit_points: hit_points} = Character.heal(@hero, 3)
+    assert hit_points == initial_hit_points + 3
+  end
+
+  test "hero hit_points dont go over max_hit_points" do
+    %Character{hit_points: hit_points, max_hit_points: max_hit_points} =
+      @hero
+      |> Character.heal(35)
+
+    assert hit_points == max_hit_points
   end
 end
